ef25f217640c7fc1f9d21140c98a2f9adeac8045
Sourhead <58874873+SOURHEAD@users.noreply.github.com>
2024-01-11 22:38:56 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template
-import requests
 
 def create_page():
     app = Flask(__name__)
@@ -15,8 +14,3 @@ if __name__ == "__main__":
     app = create_page()
     app.run(host="0.0.0.0", debug=True)
 
-def request_get(url):
-    print("Sending request to ", url)
-    response = requests.get(url)                                #connects and pings the url in its given parameter and return the status code                    
-    print("Requst code recieved is", response.status_code)      #for unit tests as we need to check if the website is up,(200-299 is fine but I just used 200)
-    return response.status_code
