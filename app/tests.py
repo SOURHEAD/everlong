e7905c75a46ba7021bb8d6793e21d4dd85044e44
Sourhead <58874873+SOURHEAD@users.noreply.github.com>
2024-01-12 00:58:01 +0530

    Update tests.py

--- app/tests.py
+++ app/tests.py
@@ -1,14 +1,26 @@
 import unittest
-import requests
-def request_get(url):
-    print("Sending request to ", url)
-    response = requests.get(url)                                #connects and pings the url in its given parameter and return the status code                    
-    print("Requst code recieved is", response.status_code)      #for unit tests as we need to check if the website is up,(200-299 is fine but I just used 200)
-    return response.status_code
+from flask import Flask, render_template
 
-class load_website(unittest.TestCase):
-    def test_website(self):
-        self.assertEqual(request_get("http://127.0.0.1:5000/"), 200)
-        
-if __name__ == "__main__":
+def create_page():                #here form the main.py file because imports just won't work with python container
+    app = Flask(__name__)
+
+    @app.route("/")
+    def hello():
+        message = "Hello, World"
+        return render_template('index.html', message=message)
+
+    return app
+
+class TestMain(unittest.TestCase):
+    
+    def setUp(self):
+        self.app = create_page()
+        self.app.testing = True
+        self.client = self.app.test_client()
+
+    def test_hello_route(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+if __name__ == '__main__':
     unittest.main()
