dcb30ba4f14d56f53ef43b3220a886e3b030b7bc
Sourhead <58874873+SOURHEAD@users.noreply.github.com>
2024-01-11 21:36:19 +0530

    Create tests.py

---
+++ app/tests.py
@@ -0,0 +1,10 @@
+import requests
+import unittest
+from main import request_get
+
+class load_website(unittest.TestCase):
+    def test_website(self):
+        self.assertEqual(request_get("http://127.0.0.1:5000/"), 200)
+        
+if __name__ == "__main__":
+    unittest.main()
